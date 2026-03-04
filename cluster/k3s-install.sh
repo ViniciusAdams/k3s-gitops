effacc9a4ea82c364e909d84b25c8deb6f5cbd1b
ViniciusAdams <viniciusadams98@gmail.com>
2026-03-04 08:29:14 -0300

    adding path to kubeconfig to avoid kind references

--- cluster/k3s-install.sh
+++ cluster/k3s-install.sh
@@ -3,10 +3,13 @@
 
 set -e
 
+K3S_KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
+
 echo "Installing k3s..."
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--bind-address=127.0.0.1" sh -
 
-echo "Waiting for node to be ready..."
+echo "Waiting for k3s node to be ready..."
+export KUBECONFIG=$K3S_KUBECONFIG
 kubectl wait --for=condition=Ready nodes --all --timeout=60s
 
 echo "Applying cluster manifests..."
